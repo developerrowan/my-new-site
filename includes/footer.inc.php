6d73eead1511fbb8a0ef8220ac51ca2e0ecddec0
Devin Rowan <its@autumnrowan.com>
2022-09-27 18:58:14 -0500

    Step 1 COMPLETE

---
+++ includes/footer.inc.php
@@ -0,0 +1,22 @@
+<aside>
+			<div class="content-frame">
+				<h3>My Hobbies</h3>
+				<ol>
+					<li>Music</li>
+					<li>Guitar</li>
+					<li>Coding</li>
+				</ol>
+				<div class="img-container">
+					<img src="images/Lighthouse.jpg" alt="A lighthouse">
+				</div>
+			</div>
+		</aside>
+	</div>
+	<footer>
+		&copy; 201? YOUR NAME
+		&nbsp;&nbsp;&nbsp;
+		<a href="privacy-policy.php">Privacy Policy</a>
+	</footer>
+	<div id="mobile-nav-button">&#9776;</div>
+</body>
+</html>
